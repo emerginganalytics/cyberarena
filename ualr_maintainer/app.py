2d2dc42b79dd8258d389c6b847a30ea46f0490f2
philiphuff <pdhuff@ualr.edu>
2019-08-30 16:33:08 -0500

    Updated email output

--- ualr_maintainer/app.py
+++ ualr_maintainer/app.py
@@ -7,6 +7,7 @@ import string
 import create_workout
 import list_vm
 import start_stop_vm
+from workoutdescription import cyberattack
 
 from flask import Flask, render_template, redirect, url_for
 from flask import jsonify
@@ -16,13 +17,15 @@ import smtplib
 
 # datastore dependency
 from google.cloud import datastore
+
 ds_client = datastore.Client()
 
 
-# create random strings --> will be used to create random workoutID 
+# create random strings --> will be used to create random workoutID
 def randomStringDigits(stringLength=6):
     lettersAndDigits = string.ascii_lowercase + string.digits
     return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+
 
 # --------------------------- FLASK APP --------------------------
 
@@ -43,9 +46,10 @@ def store_workout_info(workout_id, user_mail, workout_duration, workout_type, ti
     ds_client.put(new_user)
 
 
-
 # send email method
 def send_email(user_mail, workout_type, list_ext_IP):
+    from_address = 'philiphuff7@gmail.com'
+    from_address_cred = 'xnwaiuucpaxzsnys'
     server = smtplib.SMTP('smtp.gmail.com', 587)
     # extended simple mail transfer protocol command sent by an email
     # to identify iterself when connecting to another email server to start
@@ -54,34 +58,32 @@ def send_email(user_mail, workout_type, list_ext_IP):
     server.starttls()
     server.ehlo()
 
-    server.login('philiphuff7@gmail.com', 'xnwaiuucpaxzsnys')
+    server.login(from_address, from_address_cred)
 
-    subject = "Your UA Little Rock Cyber Gym Workout is Ready!"
-    body = "You just created a {} workout for a team of student. \n\n".format(workout_type)
+    for (ind, team_url) in enumerate(list_ext_IP):
+        body = str.replace(cyberattack.EMAIL_BODY, "WORKOUT_URL", team_url)
+        subject = "Your UA Little Rock Cyber Gym Workout %s is Ready! Forward this email to Team %d" % (
+        workout_type, str(ind + 1))
+        msg = f"Subject: {subject}\n\n{body}"
 
-    for (ind,team_url) in enumerate(list_ext_IP):
-        body += "Team {} : {} \n".format(ind+1, team_url[7:])
-
-    msg = f"Subject: {subject}\n\n{body}"
-
-    server.sendmail(
-        'philiphuff7@gmail.com',
-        user_mail,
-        msg
-    )
+        server.sendmail(
+            from_address,
+            user_mail,
+            msg
+        )
 
     print('Email has been sent')
     server.quit()
 
 
-
 # Application
 app = Flask(__name__)
+
 
 @app.route('/')
 def invalid_workout():
     return render_template('no_workout.html')
-    
+
 
 @app.route('/<workout_type>')
 def index(workout_type):
@@ -90,20 +92,18 @@ def index(workout_type):
 
 @app.route('/workout_done/<build_data>')
 def workout_done(build_data):
-    return render_template('workout_done.html', build_data = build_data)
+    return render_template('workout_done.html', build_data=build_data)
 
 
 @app.route('/listvm')
 def list_vm_instances():
-    list_vm_test = list_vm.list_instances('ualr-cybersecurity','us-central1-a')
-    return render_template('list_instances.html', list_vm = list_vm_test)
+    list_vm_test = list_vm.list_instances('ualr-cybersecurity', 'us-central1-a')
+    return render_template('list_instances.html', list_vm=list_vm_test)
 
 
 @app.route('/startvm', methods=['GET', 'POST'])
 def start_vm():
-
     if request.method == 'POST':
-
         vm = request.get_json()
         print(vm)
         print(vm['instance'])
@@ -115,9 +115,7 @@ def start_vm():
 
 @app.route('/stopvm', methods=['GET', 'POST'])
 def stop_vm():
-
     if request.method == 'POST':
-
         vm = request.get_json()
         print(vm)
         print(vm['instance'])
@@ -127,11 +125,8 @@ def stop_vm():
         return "vm stop"
 
 
-
-
 @app.route('/update', methods=['GET', 'POST'])
 def build_dos_workout():
-
     print("yo test")
 
     if request.method == 'POST':
@@ -152,13 +147,12 @@ def build_dos_workout():
 
         store_workout_info(generated_workout_ID, build_data['email'], build_data['length'], build_data['type'], ts)
 
-        for i in range (1, num_team+1):
+        for i in range(1, num_team + 1):
 
             network = 'workout-{}-{}-t{}'.format(ts, generated_workout_ID, i)
             subnetwork = 'lab-{}-{}-t{}'.format(ts, generated_workout_ID, i)
 
             if (build_data['type'] == 'dos'):
-
                 ext_IP_lab_entry = create_workout.create_dos_workout(network, subnetwork, ts)
                 list_ext_ip.append(ext_IP_lab_entry)
                 # succesData = {
@@ -170,51 +164,35 @@ def build_dos_workout():
                 # jsonify is very important for otherwise AJAX succes is not handle correctly
                 # return jsonify(succesData)
 
-
             if (build_data['type'] == 'cyberattack'):
-                
                 ext_IP_lab_entry = create_workout.create_cyberattack_workout(network, subnetwork, ts)
                 list_ext_ip.append(ext_IP_lab_entry)
-            
+
             # future work
             # jsonify is very important for otherwise AJAX succes is not handle correctly
             # return jsonify(succesData)
 
             if (build_data['type'] == 'spoof'):
-
                 ext_IP_lab_entry = create_workout.create_spoof_workout(network, subnetwork, ts)
                 list_ext_ip.append(ext_IP_lab_entry)
 
             if (build_data['type'] == 'hiddennode'):
-
                 ext_IP_lab_entry = create_workout.create_hiddennode_workout(network, subnetwork, ts)
                 list_ext_ip.append(ext_IP_lab_entry)
 
             if (build_data['type'] == 'ids'):
-
                 ext_IP_lab_entry = create_workout.create_ids_workout(network, subnetwork, ts)
                 list_ext_ip.append(ext_IP_lab_entry)
 
-
-            send_email(build_data['email'], build_data['type'], list_ext_ip)
+        send_email(build_data['email'], build_data['type'], list_ext_ip)
 
         # add time for guacamole setup for each team
         # for i in range(len(list_ext_ip)):
         #     time.sleep(60)
-        
+
         print("YOOO")
 
-
         return "DONE"
-    
-
-
-
-
-
-
-
-
 
 ### Purpose of the os.environ.get ??? --> why not just choosing a port
 # if __name__ == '__main__':
---
+++ ualr_maintainer/workoutdescription.py
@@ -0,0 +1,6 @@
+
+class cyberattack:
+    EMAIL_BODY = """
+                Welcome to your team's cyber attack workout, in which you will experience how a cyber-attack
+                 known as a botnet. Your server should be ready at the following website: WORKOUT_URL
+            """
