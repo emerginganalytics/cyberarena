ca7ef93376ed6e742bd579938ec839d278e41485
abbomberger <abbomberger@users.noreply.github.com>
2019-09-20 11:35:32 -0500

    Adding Workouts ...

--- ualr_maintainer/workoutdescription.py
+++ ualr_maintainer/workoutdescription.py
@@ -3,10 +3,16 @@ def body_workout_message(workout_type, team_url):
     print(workout_type)
 
     if (workout_type == "cyberattack"):
-        message = "Welcome to your team's cyber attack workout, in which you will experience how a cyber-attack known as a botnet. Your server should be ready at the following website: {}".format(team_url)
+        message = "Welcome to your team's cyber attack workout, in which you will experience how a cyber-attack known as a botnet works. Your server should be ready at the following website: {}".format(team_url)
     
     if (workout_type == "dos"):
         message = "Test dos message workout {}".format(team_url)
+
+    if (workout_type == "phishing"):
+        message = "Welcome to your team's phishing workout in which you will learn how a phishing attack works and how to do avoid them. Your server should be ready at the following website: {}".format(team_url)
+
+    if (workout_type == "theharbor"):
+        message = "Welcome to TheHarbor! Here you will learn a brief demonstration of a firewall at work. Block the correct ports and you might find a flag hidden in the site. You server should be ready at the following website: {}".format(team_url)
 
     return message
 
