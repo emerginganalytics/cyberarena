25af1456d3604b9a2ba49a13c839b6e7049c57c9
philiphuff <pdhuff@ualr.edu>
2019-11-27 09:57:12 -0600

    Significant update to naming convention and deletion
    The Google Cloud API only allows wildcards at the end of the string when pulling back resources. So in order for the deletion routine to work, everything needs to lead with the random workout_id.

--- ualr_maintainer/delete_workout.py
+++ ualr_maintainer/delete_workout.py
@@ -1,132 +1,99 @@
 #
 # This function is intended to be run in Google Cloud Functions as the handler to the maintenance pub/sub topic
 # Need to rewrite to delete based on expired workout and not loop through all resources each time.
+# Use this to filter based on project labels.workoutid = sd89wj when querying
+# e.g. compute.instances().list(project=project, zone=zone, filter='labels.workoutid = sd89wj').execute()
 #
 
 import googleapiclient.discovery
 from datetime import datetime, timedelta, date
 from google.cloud import datastore
 
+# Global variables for this function
 ds_client = datastore.Client()
 compute = googleapiclient.discovery.build('compute', 'v1')
-
-
-# IN PROGRESS
-# to be update with expiration date query from datastore
-def isReadyToDelete(created_date):
-    now = datetime.now()
-    print(now)
-    intance_creation_date = datetime.strptime(created_date[:19], '%Y-%m-%dT%H:%M:%S')
-    print(intance_creation_date)
-    delta = now - intance_creation_date
-    print(delta.days)
-
-
-# retrieve all workout rows information
-list_info_workout = []
-list_workout_id = []
-
-
-def retrieve_workout_info():
-    query_user = ds_client.query(kind='cybergym-workout')
-    for workout in list(query_user.fetch()):
-        list_info_workout.append(workout)
-        list_workout_id.append(workout['workout_ID'])
-
-
-# store the vm into a list
-list_vm_workout = []
-
-
-def retrieve_all_vm(compute, project, zone):
-    result = compute.instances().list(project=project, zone=zone).execute()
-    try:
-        for vm_instance in result['items']:
-            list_vm_workout.append(vm_instance)
-    except():
-        print("No VM found")
-
-
-# store the different network
-list_network = []
-
-
-def retrieve_all_network(compute, project):
-    try:
-        result = compute.networks().list(project=project).execute()
-        for network in result['items']:
-            list_network.append(network)
-    except():
-        print("No Network found")
-
-
-# store the different network
-list_subnetwork = []
-def retrieve_all_subnetwork(compute, project, region):
-    try:
-        response = compute.subnetworks().list(project=project, region=region).execute()
-        for subnetwork in response['items']:
-            list_subnetwork.append(subnetwork)
-    except():
-        print("No subnetworks found")
-
-
-# store the different network
-list_firewall_rules = []
-def retrieve_all_firewall_rules(compute, project):
-    try:
-        response = compute.firewalls().list(project=project).execute()
-        for fw_rule in response['items']:
-            list_firewall_rules.append(fw_rule)
-    except():
-        print("No firewall rules found")
-
-
+expired_workout = []
 project = 'ualr-cybersecurity'
 zone = 'us-central1-a'
 region = 'us-central1'
 
 
-# retrieve vm and query from datastore
-retrieve_workout_info()
-retrieve_all_vm(compute, project, zone)
-retrieve_all_network(compute, project)
-retrieve_all_subnetwork(compute, project, region)
-retrieve_all_firewall_rules(compute, project)
+# IN PROGRESS
+# to be update with expiration date query from datastore
+def workout_age(created_date):
+    now = datetime.now()
+    instance_creation_date = datetime.fromtimestamp(int(created_date))
+    delta = now - instance_creation_date
+    return delta.days
 
 
-# delete matching vm
-for vm in list_vm_workout:
-    for w_id in list_workout_id:
-        if w_id in vm["name"]:
-            print("Delete VM : ", vm["name"])
-            request = compute.instances().delete(project=project, zone=zone,
-                                                 instance=vm["name"])
-            response = request.execute()
+def delete_vms(workout_id):
+    result = compute.instances().list(project=project, zone=zone, filter='name = {}*'.format(workout_id)).execute()
+    try:
+        if 'items' in result:
+            for vm_instance in result['items']:
+                compute.instances().delete(project=project, zone=zone,
+                                           instance=vm_instance["name"]).execute()
+        else:
+            print("No Virtual Machines to delete for workout %s" % workout_id)
+    except():
+        print("Error in deleting VM for %s" % workout_id)
 
 
-# delete matching firewall-rules
-for fw_rule in list_vm_workout:
-    for w_id in list_workout_id:
-        if (w_id in fw_rule["name"]):
-            print("Delete Firewall Rule : ", fw_rule["name"])
-            request = compute.firewalls().delete(project=project, instance=fw_rule["name"])
-            response = request.execute()
+def delete_firewall_rules(workout_id):
+    try:
+        result = compute.firewalls().list(project=project, filter='name = {}*'.format(workout_id)).execute()
+        if 'items' in result:
+            for fw_rule in result['items']:
+                compute.firewalls().delete(project=project, instance=fw_rule["name"]).execute()
+    except():
+        print("Error in deleting firewall rules for %s" % workout_id)
 
 
-# delete matching subnetworks
-for subnetwork in list_subnetwork:
-    for w_id in list_workout_id:
-        if (w_id in subnetwork["name"]):
-            print("Delete network : ", subnetwork["name"])
-            request = compute.subnetworks().delete(project=project, region=region, subnetwork=subnetwork["name"])
-            response = request.execute()
+def delete_subnetworks(workout_id):
+    try:
+        result = compute.subnetworks().list(project=project, region=region,
+                                              filter='name = {}*'.format(workout_id)).execute()
+        if 'items' in result:
+            for subnetwork in result['items']:
+                compute.subnetworks().delete(project=project, region=region,
+                                                       subnetwork=subnetwork["name"]).execute()
+    except():
+        print("Error in deleting subnetworks for %s" % workout_id)
 
 
-# delete matching networks
-for network in list_network:
-    for w_id in list_workout_id:
-        if (w_id in network["name"]):
-            print("Delete network : ", network["name"])
-            request = compute.networks().delete(project=project, network=network["name"])
-            response = request.execute()
+def delete_network(workout_id):
+    try:
+        result = compute.networks().list(project=project, filter='name = {}*'.format(workout_id)).execute()
+        if 'items' in result:
+            for network in result['items']:
+                compute.networks().delete(project=project, network=network["name"]).execute()
+    except():
+        print("Error in deleting network for %s" % workout_id)
+
+
+
+result = compute.subnetworks().list(project=project, region=region,
+                                      filter='name = lab*').execute()
+for subnetwork in result['items']:
+    compute.subnetworks().delete(project=project, region=region,
+                                           subnetwork=subnetwork["name"]).execute()
+
+result = compute.networks().list(project=project, filter='name = workout*').execute()
+for network in result['items']:
+    compute.networks().delete(project=project, network=network["name"]).execute()
+
+
+query_workouts = ds_client.query(kind='cybergym-workout')
+query_workouts.add_filter("timestamp", "<", str(datetime.now() - timedelta(days=30)))
+for workout in list(query_workouts.fetch()):
+    if 'resources_deleted' not in workout:
+        workout['resources_deleted'] = False
+    if workout_age(workout['timestamp']) >= int(workout['expiration']) and not workout['resources_deleted']:
+        expired_id = workout['workout_ID']
+        delete_vms(expired_id)
+        delete_firewall_rules(expired_id)
+        delete_subnetworks(expired_id)
+        delete_network(expired_id)
+        workout['resources_deleted'] = True
+        ds_client.put(workout)
