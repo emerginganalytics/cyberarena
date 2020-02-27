36c515e789c46d51df590759bceca184ca4bcf2f
Mark Barnes <msbarnes@ualr.edu>
2020-02-27 13:37:15 -0600

    Added cloud storage bucket function

---
+++ ualr_maintainer/cloud-functions/cloud_bucket_functions.py
@@ -0,0 +1,18 @@
+from globals import storage_client
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_file_name = "local/path/to/file"
+    # destination_blob_name = "storage-object-name"
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
