2030643e02458153d233e538f8dcdf17c62d52f3
philiphuff <pdhuff@ualr.edu>
2020-03-04 05:10:46 -0600

    Fixed a few bugs in stop_workout and the cloud functions

--- ualr_maintainer/start_workout.py
+++ ualr_maintainer/start_workout.py
@@ -6,6 +6,7 @@ import googleapiclient.discovery
 from globals import ds_client, project, compute, dnszone, workout_globals, dns_suffix
 import time
 import calendar
+from googleapiclient.errors import HttpError
 
 # Global variables for this function
 expired_workout = []
@@ -29,21 +30,31 @@ def register_workout_update(project, dnszone, workout_id, old_ip, new_ip):
             }
         ],
         "additions": [
-        {
-            "kind": "dns#resourceRecordSet",
-            "name": workout_id + dns_suffix + ".",
-            "rrdatas": [new_ip],
-            "type": "A",
-            "ttl": 30
-        }
+            {
+                "kind": "dns#resourceRecordSet",
+                "name": workout_id + dns_suffix + ".",
+                "rrdatas": [new_ip],
+                "type": "A",
+                "ttl": 30
+            }
     ]}
 
-    request = service.changes().create(project=project, managedZone=dnszone, body=change_body)
-    response = request.execute()
-
+    # Try first to perform the DNS change, but in case the DNS did not previously exist, try again without the deletion change.
+    try:
+        service
+        request = service.changes().create(project=project, managedZone=dnszone, body=change_body).execute()
+    except HttpError:
+        try:
+            del change_body["deletions"]
+            request = service.changes().create(project=project, managedZone=dnszone, body=change_body).execute()
+        except HttpError:
+            # Finally, it may be the DNS has already been successfully updated, in which case
+            # the API call will throw an error. We ignore this case.
+            pass
 
     workout["external_ip"] = new_ip
-    workout['running'] = True
+    if workout['running'] == False:
+        workout['running'] = True
     workout['start_time'] = str(calendar.timegm(time.gmtime()))
     ds_client.put(workout)
 
