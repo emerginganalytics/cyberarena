9bfb86da26a0786a51d2287fbe82b9dbdf2dd17c
Mark Barnes <msbarnes@ualr.edu>
2020-03-06 13:30:50 -0600

    Fixed errors in YAML files

--- ualr_maintainer/cloud-functions/cloud_bucket_functions.py
+++ ualr_maintainer/cloud-functions/cloud_bucket_functions.py
@@ -1,12 +1,23 @@
-from globals import storage_client
+from globals import storage_client, workout_globals
+from yaml import load, Loader
+import os
 
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
+# get bucket with name
+bucket = storage_client.get_bucket(workout_globals.yaml_bucket)
+# get bucket data as blob
+blob = bucket.get_blob(workout_globals.yaml_folder + 'cs4360.yaml')
+# convert to string
+yaml_from_bucket = blob.download_as_string()
+y = load(yaml_from_bucket, Loader=Loader)
+a=1
+
+def upload_yaml(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     # bucket_name = "your-bucket-name"
     # source_file_name = "local/path/to/file"
     # destination_blob_name = "storage-object-name"
 
-    bucket = storage_client.bucket(bucket_name)
+    bucket = storage_client.get_bucket(workout_globals.yaml_bucket)
     blob = bucket.blob(destination_blob_name)
 
     blob.upload_from_filename(source_file_name)
