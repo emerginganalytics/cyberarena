dbb910893c655b194c7627d4c39608cf446da7ad
Mark Barnes <msbarnes@ualr.edu>
2020-04-01 19:48:03 -0500

    changes to pub/sub test message

--- ualr_maintainer/app.py
+++ ualr_maintainer/app.py
@@ -668,7 +668,7 @@ def publish():
         topic = request.form['topic']
         workout_id = request.form['workout_id']
         publish_client = pubsub_v1.PublisherClient()
-        publish_client.publish(topic, b'This is a test', test='true')
+        publish_client.publish(topic, b'%s-workout complete!' % (workout_id), workout_id=workout_id)
     return redirect("/landing/%s" % (workout_id))
 
 if __name__ == '__main__':
