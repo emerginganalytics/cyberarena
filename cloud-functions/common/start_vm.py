eef5298db35a782c05547763f46ae40a8cabd6cd
SWillis31 <34135272+SWillis31@users.noreply.github.com>
2020-05-20 04:56:20 -0500

    More CF stuff

--- cloud-functions/common/start_vm.py
+++ cloud-functions/common/start_vm.py
@@ -1,9 +1,60 @@
-from common.globals import project, dnszone, ds_client, storage_client, workout_globals, compute
+#
+# This script is intended to be copied into the landing page application for the button click when
+# resuming a workout which has previously been stopped.
+#
+import googleapiclient.discovery
+from globals import ds_client, project, compute, dnszone, workout_globals, dns_suffix, logger
+import time
+import calendar
+from googleapiclient.errors import HttpError
 
-def start_vm(workout_id):
-    zone = 'us-central1-a'
-    region = 'us-central1'
+# Global variables for this function
+expired_workout = []
+zone = 'us-central1-a'
+region = 'us-central1'
 
+
+# Create a new DNS record for the server and add the information to the datastore for later management
+def register_workout_update(project, dnszone, workout_id, new_ip):
+    service = googleapiclient.discovery.build('dns', 'v1')
+
+    # First, get the existing workout DNS
+    response = service.resourceRecordSets().list(project=project, managedZone=dnszone,
+                                              name=workout_id + dns_suffix + ".").execute()
+    existing_rrset = response['rrsets']
+    change_body = {
+        "deletions": existing_rrset,
+        "additions": [
+            {
+                "kind": "dns#resourceRecordSet",
+                "name": workout_id + dns_suffix + ".",
+                "rrdatas": [new_ip],
+                "type": "A",
+                "ttl": 30
+            }
+    ]}
+
+    # Try first to perform the DNS change, but in case the DNS did not previously exist, try again without the deletion change.
+    try:
+        request = service.changes().create(project=project, managedZone=dnszone, body=change_body).execute()
+    except HttpError:
+        try:
+            del change_body["deletions"]
+            request = service.changes().create(project=project, managedZone=dnszone, body=change_body).execute()
+        except HttpError:
+            # Finally, it may be the DNS has already been successfully updated, in which case
+            # the API call will throw an error. We ignore this case.
+            pass
+
+    # Now update the parameters in the workout object
+    key = ds_client.key('cybergym-workout', workout_id)
+    workout = ds_client.get(key)
+    workout["external_ip"] = new_ip
+
+    ds_client.put(workout)
+
+
+def start_workout(workout_id):
     print("Starting workout %s" % workout_id)
     key = ds_client.key('cybergym-workout', workout_id)
     workout = ds_client.get(key)
