1846787d9ef402866c75bbbbfd9100a0d905e8f9
SWillis31 <34135272+SWillis31@users.noreply.github.com>
2020-06-05 13:18:21 -0500

    Added Shodan workout files

---
+++ labs/Shodan/routes.py
@@ -0,0 +1,70 @@
+from flask import abort, jsonify, redirect, render_template, request, escape
+
+import shodan
+import shodan.helpers as helpers
+import sys
+import json
+
+from app import app
+
+# SHODAN_API_KEY = 'BWmELJlT4jH83fFG29sKiKqwouKk7kL8'
+SHODAN_API_KEY = 'N81y8YZTBIUfVmrEa1ZL8ywi119Pz1pT'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def default():
+    api = shodan.Shodan(SHODAN_API_KEY)
+    result_services = []
+    result_count = 0
+    page_template = 'index.jinja'
+    if request.method == 'POST':
+        query = request.form.get('shodan_query')
+        page_num = request.form.get('page_number')
+        result = api.search(query, limit=10)
+        result_count = api.count(query)
+        print(result.keys())
+        # for result in api.search_cursor(query):
+        #     result_services.append(result)
+
+
+        return render_template(page_template, shodanResults=result, resultCount=result_count, query=query, page_num=page_num)
+    return render_template(page_template)
+
+
+
+
+@app.route('/results/<string:ip>')
+def result(ip):
+    api = shodan.Shodan(SHODAN_API_KEY)
+    ip_list = []
+    ip_list.append(ip)
+    info = api.host(ip_list, history=True)
+    for tmp in info["data"]:
+        has_screenshot = helpers.get_screenshot(tmp)
+        if has_screenshot is not None:
+            info["screenshot"] = has_screenshot["data"]
+
+    page_template = 'results.jinja'
+
+    return render_template(page_template, resultInfo=info)
+
+
+@app.route('/data/', methods=['GET', 'POST'])
+def data():
+    page_template = 'raw_data.jinja'
+    if request.method == "POST":
+        print("POST")
+        return render_template(page_template, rawData=request.form.get('data'))
+    if request.method == "GET":
+        print("GET")
+        print(type(request.get_data()))
+
+    # return render_template(page_template, rawData=request.get_json())
+    return "failed"
+
+
+@app.route('/query/<string:query>/<int:page>')
+def view_all_query_results(query, page):
+    api = shodan.Shodan(SHODAN_API_KEY)
+    if request.method == "POST":
+        result = api.search(query, page=page)
