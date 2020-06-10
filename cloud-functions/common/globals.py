cafb5c06cb3a861905963bf669413ad8cefc4a88
philiphuff <pdhuff@ualr.edu>
2020-06-10 10:59:25 -0500

    Using setx to set environment variables for all users

--- cloud-functions/common/globals.py
+++ cloud-functions/common/globals.py
@@ -20,8 +20,8 @@ class workout_globals():
     MAX_RUN_HOURS = 10
     yaml_bucket = project + '_cloudbuild'
     yaml_folder = 'yaml-build-files/'
-    windows_startup_script_env = 'set WORKOUTID={env_workoutid}\n'
-    windows_startup_script_task = 'set WORKOUTKEY{q_number}={env_workoutkey}\n' \
+    windows_startup_script_env = 'setx /m WORKOUTID {env_workoutid}\n'
+    windows_startup_script_task = 'setx /m WORKOUTKEY{q_number} {env_workoutkey}\n' \
                                   'call gsutil cp ' + script_repository + '{script} .\n' \
                                   'schtasks /Create /SC MINUTE /TN {script_name} /RU System /TR {script_command}'
     linux_startup_script_env = '#! /bin/bash\nexport WORKOUTID={env_workoutid}\n(crontab -l 2>/dev/null; echo "*/5 * * * * /usr/bin/{script}") | crontab -'
