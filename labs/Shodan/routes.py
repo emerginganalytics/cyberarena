350ee2fb8f0d66733e6532f2a4626d80d9d311a8
abbomberger <abbomberger@users.noreply.github.com>
2020-06-16 15:29:11 -0500

    Updating Shodan Workout

--- labs/Shodan/routes.py
+++ labs/Shodan/routes.py
@@ -1,4 +1,5 @@
 from flask import abort, jsonify, redirect, render_template, request, escape
+from google.cloud import datastore
 
 import shodan
 import shodan.helpers as helpers
@@ -9,77 +10,122 @@ from app import app
 
 # SHODAN_API_KEY = 'BWmELJlT4jH83fFG29sKiKqwouKk7kL8'
 SHODAN_API_KEY = 'N81y8YZTBIUfVmrEa1ZL8ywi119Pz1pT'
+ds_client = datastore.Client()
+project = 'ualr-cybersecurity'
 
 
-# TODO: Need to implement error catching for bad queries
-@app.route('/', methods=['GET', 'POST'])
-def default():
+@app.route('/<workout_id>', methods=['GET', 'POST'])
+def default(workout_id):
     api = shodan.Shodan(SHODAN_API_KEY)
-    result_services = []
-    result_count = 0
     page_template = 'index.jinja'
-    if request.method == 'POST':
-        try:
-            query = request.form.get('shodan_query')
-            page_num = request.form.get('page_number')
-            result = api.search(query, limit=10)
-            result_count = api.count(query)
-            print(result.keys())
-            # for result in api.search_cursor(query):
-            #     result_services.append(result)
 
-            return render_template(
-                page_template,
-                shodanResults=result,
-                resultCount=result_count,
-                query=query,
-                page_num=page_num
-            )
-        except shodan.APIError as e:
-            print(e)
-            e_template = 'invalid_query.jinja'
-            return render_template(e_template)
-    return render_template(page_template)
+    workout = ds_client.get(ds_client.key('cybergym-workout', workout_id))
+
+    if workout:
+        if workout['type'] == 'shodan':
+            if request.method == 'POST':
+                try:
+                    query = request.form.get('shodan_query')
+                    page_num = request.form.get('page_number')
+                    result = api.search(query, limit=10)
+                    result_count = api.count(query)
+                    print(result.keys())
+
+                    return render_template(
+                        page_template,
+                        shodanResults=result,
+                        resultCount=result_count,
+                        query=query,
+                        page_num=page_num,
+                        workoutid=workout_id,
+                    )
+                except shodan.APIError as e:
+                    print(e)
+                    e_template = 'invalid_query.jinja'
+                    return render_template(e_template)
+            return render_template(page_template)
+        else:
+            return redirect('/invalid/' + workout_id)
+    else:
+        return redirect('/invalid/' + workout_id)
 
 
-@app.route('/results/<string:ip>')
-def result(ip):
+@app.route('/results/<string:ip>/<workout_id>')
+def result(ip, workout_id):
     api = shodan.Shodan(SHODAN_API_KEY)
-    ip_list = []
-    ip_list.append(ip)
-    info = api.host(ip_list, history=True)
-    for tmp in info["data"]:
-        has_screenshot = helpers.get_screenshot(tmp)
-        if has_screenshot is not None:
-            info["screenshot"] = has_screenshot["data"]
+    workout = ds_client.get(ds_client.key('cybergym-workout', workout_id))
 
-    page_template = 'results.jinja'
+    if workout:
+        if workout['type'] == 'shodan':
 
-    return render_template(page_template, resultInfo=info)
+            ip_list = []
+            ip_list.append(ip)
+            info = api.host(ip_list, history=True)
+            for tmp in info["data"]:
+                has_screenshot = helpers.get_screenshot(tmp)
+                if has_screenshot is not None:
+                    info["screenshot"] = has_screenshot["data"]
+
+            page_template = 'results.jinja'
+
+            return render_template(page_template, resultInfo=info, workoutid=workout_id,)
+        else:
+            return redirect('/invalid/' + workout_id)
+    else:
+        return redirect('/invalid/' + workout_id)
 
 
-@app.route('/data/', methods=['GET', 'POST'])
-def data():
+@app.route('/data/<workout_id>', methods=['GET', 'POST'])
+def data(workout_id):
     page_template = 'raw_data.jinja'
-    if request.method == "POST":
-        print("POST")
-        return render_template(page_template, rawData=request.form.get('data'))
-    if request.method == "GET":
-        print("GET")
-        print(type(request.get_data()))
+    workout = ds_client.get(ds_client.key('cybergym-workout', workout_id))
 
-    # return render_template(page_template, rawData=request.get_json())
-    return "failed"
+    if workout:
+        if workout['type'] == 'shodan':
+            if request.method == "POST":
+                print("POST")
+                return render_template(page_template, rawData=request.form.get('data'), workoutid=workout_id,)
+            if request.method == "GET":
+                print("GET")
+                print(type(request.get_data()))
+        else:
+            return redirect('/invalid/' + workout_id)
+    else:
+        return redirect('/invalid/' + workout_id)
 
 
-@app.route('/query/<string:query>/<int:page>')
-def view_all_query_results(query, page):
+@app.route('/query/<string:query>/<int:page>/<workout_id>')
+def view_all_query_results(query, page, workout_id):
     api = shodan.Shodan(SHODAN_API_KEY)
-    if request.method == "POST":
-        result = api.search(query, page=page)
+    workout = ds_client.get(ds_client.key('cybergy-workout', workout_id))
+
+    if workout:
+        if workout['type'] == 'shodan':
+            if request.method == "POST":
+                result = api.search(query, page=page)
+        else:
+            return redirect('/invalid/' + workout_id)
+    else:
+        return redirect('/invalid/' + workout_id)
+
+
+# This path is only necessary if dynamically creating assessment questions
+# otherwise, ignore
+@app.route('/loader/<workout_id>')
+def loader(workout_id):
+    key = ds_client.key('cybergym-workout', workout_id)
+    workout = ds_client.get(key)
+
+    if workout:
+        if workout['type'] == 'shodan':
+            return redirect('/' + workout_id)
+        else:
+            return redirect('/invalid/' + workout_id)
+    else:
+        return redirect('/invalid/' + workout_id)
 
 
 @app.route('/invalid/<workout_id>')
 def invalid(workout_id):
-    page_template = 'invalid.html'
+    page_template = 'invalid_workout.html'
     return render_template(page_template)
