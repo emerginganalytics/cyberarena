acb05687e8babf5d5e66f7e327480021740393f1
SWillis31 <34135272+SWillis31@users.noreply.github.com>
2020-06-24 03:38:20 -0500

    Small build fixes/landing page edits

--- ualr_maintainer/forms.py
+++ ualr_maintainer/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, PasswordField
 from wtforms.validators import DataRequired, Email
 
 class CreateWorkoutForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     unit = StringField('Lesson Name (for future reference)', validators=[DataRequired()])
-    team = IntegerField('Select number of teams (between 1 and 10)', validators=[DataRequired()])
+    team = IntegerField('Select number of students (between 1 and 10)', validators=[DataRequired()])
     length = IntegerField('Select length of availability (between 1 and 100 days)', validators=[DataRequired()])
 
 class CreateExpoForm(FlaskForm):
