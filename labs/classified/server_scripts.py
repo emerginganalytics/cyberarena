fb3d8acd0c42ea27daa0126ae5ae309d07b69659
Chance Melby <camelby@ualr.edu>
2020-06-25 16:34:08 -0500

    update for 2fa, wireshark, xss, dos, container, assessment, and flag generator

--- labs/classified/server_scripts.py
+++
@@ -1,9 +0,0 @@
-from google.cloud import datastore
-
-ds_client = datastore.Client()
-project = 'ualr-cybersecurity'
-
-
-def set_workout_flag(workout_id):
-    flag = ds_client.get(ds_client.key('cybergym-workout', workout_id))
-    ds_client.put(flag)
