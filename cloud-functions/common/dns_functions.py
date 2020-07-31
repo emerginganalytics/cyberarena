157e163bd34d239efbf59b81b3a73ea2080373fb
Philip Huff <pdhuff@ualr.edu>
2020-07-31 04:04:29 -0500

    Fixing DNS issues and guacamole readiness issues with the server start

--- cloud-functions/common/dns_functions.py
+++ cloud-functions/common/dns_functions.py
@@ -1,28 +1,36 @@
 import googleapiclient.discovery
 from common.globals import ds_client, dns_suffix, project, dnszone
+from googleapiclient.errors import HttpError
 
-# Create a new DNS record for the server and
-def add_dns_record(build_id, ip_address):
+
+# Create a new DNS record for the server and add the information to the datastore for later management
+def add_dns_record(build_id, new_ip):
     service = googleapiclient.discovery.build('dns', 'v1')
 
-    change_body = {"additions": [
-        {
-            "kind": "dns#resourceRecordSet",
-            "name": build_id + dns_suffix + ".",
-            "rrdatas": [ip_address],
-            "type": "A",
-            "ttl": 30
-        }
+    # First, get the existing workout DNS
+    response = service.resourceRecordSets().list(project=project, managedZone=dnszone,
+                                              name=build_id + dns_suffix + ".").execute()
+    existing_rrset = response['rrsets']
+    change_body = {
+        "deletions": existing_rrset,
+        "additions": [
+            {
+                "kind": "dns#resourceRecordSet",
+                "name": build_id + dns_suffix + ".",
+                "rrdatas": [new_ip],
+                "type": "A",
+                "ttl": 30
+            }
     ]}
 
-    request = service.changes().create(project=project, managedZone=dnszone, body=change_body)
-    response = request.execute()
-
-
-# Add the information to the datastore for later management.
-# We are deprecating this as we build out the guacamole automation.
-def register_workout_server(workout_id, server, guac_path):
-    key = ds_client.key('cybergym-workout', workout_id)
-    workout = ds_client.get(key)
-    workout["servers"].append({"server": server, "guac_path": guac_path})
-    ds_client.put(workout)
+    # Try first to perform the DNS change, but in case the DNS did not previously exist, try again without the deletion change.
+    try:
+        request = service.changes().create(project=project, managedZone=dnszone, body=change_body).execute()
+    except HttpError:
+        try:
+            del change_body["deletions"]
+            request = service.changes().create(project=project, managedZone=dnszone, body=change_body).execute()
+        except HttpError:
+            # Finally, it may be the DNS has already been successfully updated, in which case
+            # the API call will throw an error. We ignore this case.
+            pass
