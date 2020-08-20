3c5e9ecb055dd14a61160947989b439b918ced7c
abbomberger <abbomberger@users.noreply.github.com>
2020-08-20 10:43:26 -0500

    Cleaning Up Old Files. Fixing Teacher Instructions to Open to New Tab.

--- labs/simple-hash.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-cont = "y"
-while cont == "y":
-    password = input("\nPassword: ")
-    hashed_password_md5 = hashlib.md5(str(password).encode("UTF-8")).hexdigest()
-    print("md5:\t", hashed_password_md5)
-    cont = input("\nContinue? (y/n): ")
