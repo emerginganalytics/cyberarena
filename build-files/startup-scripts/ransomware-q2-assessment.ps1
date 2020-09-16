304dae2b4e1b8bc99a121196ff69b8bab52fd527
Philip Huff <pdhuff@ualr.edu>
2020-09-16 10:21:47 -0500

    Fixed a powershell script error used for assessment

--- build-files/startup-scripts/ransomware-q2-assessment.ps1
+++ build-files/startup-scripts/ransomware-q2-assessment.ps1
@@ -10,5 +10,5 @@ $Protection.ControlledFolderAccessProtectedFolders -eq $ProtectedFolder)
         "workout_id"=$env:WORKOUTID;
         "token"=$env:WORKOUTKEY1;
     }
-    Invoke-WebRequest -UseBasicParsing "$env:URL/complete" -Method Post -Body $workout -ContentType "application/json"
+    Invoke-WebRequest -UseBasicParsing "$env:URL/complete" -Method Post -Body ($workout|ConvertTo-Json) -ContentType "application/json"
 }
