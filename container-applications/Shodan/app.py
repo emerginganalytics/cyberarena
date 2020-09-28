c9b5b47c56eae962d976843b4bc0023243ee23ff
Andrew <50710945+abbomberger@users.noreply.github.com>
2020-09-28 13:39:07 -0500

    Update app.py

--- container-applications/Shodan/app.py
+++ container-applications/Shodan/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+from server_scripts import SHODAN_API_KEY
 
 app = Flask(__name__, template_folder='templates')
-SHODAN_API_KEY = 'zs72fHys8zo5n58zWOsHLJ8kLHOTBtWt'
 import routes
 import os
 
 if __name__ == '__main__':
-    os.environ['SHODAN_API_KEY'] = 'zs72fHys8zo5n58zWOsHLJ8kLHOTBtWt'
+    os.environ['SHODAN_API_KEY'] = SHODAN_API_KEY
     app.run(host='0.0.0.0', port=5000)
