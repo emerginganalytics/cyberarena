dcc110a78f37475ca4e3cb22852cda249e6f3008
abbomberger <abbomberger@users.noreply.github.com>
2020-10-01 10:08:48 -0500

    Fixes to Shodan workout ...

--- container-applications/Shodan/app.py
+++ container-applications/Shodan/app.py
@@ -1,10 +1,9 @@
-from flask import Flask
-
-app = Flask(__name__, template_folder='templates')
-SHODAN_API_KEY = 'zs72fHys8zo5n58zWOsHLJ8kLHOTBtWt'
 import routes
 import os
 
+from flask import Flask
+
+app = Flask(__name__, template_folder='templates')
+
 if __name__ == '__main__':
-    os.environ['SHODAN_API_KEY'] = 'zs72fHys8zo5n58zWOsHLJ8kLHOTBtWt'
     app.run(host='0.0.0.0', port=5000)
