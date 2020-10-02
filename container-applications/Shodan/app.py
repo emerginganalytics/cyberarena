346afefebf49fa16ccbd6b5303b79376a16f7bee
abbomberger <abbomberger@users.noreply.github.com>
2020-10-02 17:22:08 -0500

    Error Fixes

--- container-applications/Shodan/app.py
+++ container-applications/Shodan/app.py
@@ -1,9 +1,9 @@
-import routes
-import os
-
 from flask import Flask
 
 app = Flask(__name__, template_folder='templates')
+
+import routes
+import os
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
