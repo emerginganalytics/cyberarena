f6ffaf6573e184c3693b78eac6864c7c8657063b
Chance Melby <camelby@ualr.edu>
2020-10-26 13:57:00 -0500

    yaml and workout_id update

--- container-applications/Arena Snake/app.py
+++ container-applications/Arena Snake/app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
+from google.cloud import datastore, runtimeconfig
 import os
 
 # application instance
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
 
+ds_client = datastore.Client()
+runtimeconfig_client = runtimeconfig.Client()
+myconfig = runtimeconfig_client.config('cybergym')
+project = myconfig.get_variable('project').value.decode("utf-8")
+dns_suffix = myconfig.get_variable('dns_suffix').value.decode("utf-8")
 
-@app.route('/arena_snake')
-def arena_snake():
-    page_template = 'arena_snake.html'
-    return render_template(page_template)
+
+@app.route('/arena_snake/<workout_id>')
+def arena_snake(workout_id):
+    key = ds_client.key('cybergym-workout', workout_id)
+    workout = ds_client.get(key)
+    if workout['type'] == 'arena_snake':
+        page_template = 'arena_snake.html'
+        return render_template(page_template, workout_id=workout_id)
+    else:
+        return redirect(404)
 
 
 @app.route('/snake_flag', methods=['POST'])
