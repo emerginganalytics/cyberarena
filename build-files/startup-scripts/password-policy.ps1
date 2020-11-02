f16b396ba9017ce67af26a20b0dc2b8763f21d8e
Zac Long <zxlong@ualr.edu>
2020-11-02 13:17:46 -0600

    Fixed password-policy

--- build-files/startup-scripts/password-policy.ps1
+++ build-files/startup-scripts/password-policy.ps1
@@ -1,13 +1,10 @@
-Get-ADDefaultDomainPasswordPolicy | Out-File 'C:\Users\workout\file.txt'
-if((Get-Content 'C:\Users\workout\file.txt')[2] -like '*True*' -and
-(Get-Content 'C:\Users\workout\file.txt')[9] -like '*12*' -and
-(Get-Content 'C:\Users\workout\file.txt')[7] -like '*365*')
-{
+﻿if((((Get-ADDefaultDomainPasswordPolicy).MinPasswordLength) -eq 12) -and (((Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge.Days) -eq 365) -and (((Get-ADDefaultDomainPasswordPolicy).ComplexityEnabled) -eq "True")){
+
     $workout = @{
         workout_id=$env:WORKOUTID;
         token=$env:WORKOUTKEY0;
     }| ConvertTo-Json
     $complete_url = -join("https://buildthewarrior", $env:DNS_SUFFIX, "/complete");
     Invoke-WebRequest -UseBasicParsing $complete_url -Method POST -Body $workout -ContentType "application/json"
+
 }
-Remove-Item 'C:\Users\workout\file.txt'
