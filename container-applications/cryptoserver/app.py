5e3ff5b93766626f3a0ce9a5496155890feec3a8
Philip Huff <pdhuff@ualr.edu>
2020-11-02 16:18:02 -0600

    CYBERGYM-48 #comment simplified assessment to only update the datastore questions as complete. #resolve

--- container-applications/cryptoserver/app.py
+++ container-applications/cryptoserver/app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
+from routes import *
 
-import routes
-import os
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get(port=5000)))
+    app.run(debug=True, host='0.0.0.0', port=5000)
