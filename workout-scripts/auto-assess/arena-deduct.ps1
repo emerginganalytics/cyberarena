f3cda50a0a0071642067215e50d092b9637c2af1
Philip Huff <pdhuff@ualr.edu>
2020-11-30 06:36:27 -0600

    Update arena-deduct.ps1

--- workout-scripts/auto-assess/arena-deduct.ps1
+++ workout-scripts/auto-assess/arena-deduct.ps1
@@ -1,5 +1,5 @@
 ﻿function Invoke-Arena1-Deduct () {
   $deductworkout = @{"build_type"="arena"; "workout_id"=$env:WORKOUTID; "action"="deduct-points";}
-  Invoke-WebRequest -UseBasicParsing "$env:URL/arena-functions" -Method Post -Body $deductworkout -ContentType "application/json"
+  Invoke-WebRequest -UseBasicParsing "$env:URL/arena-functions" -Method Post -Body ($deductworkout|ConvertTo-Json) -ContentType "application/json"
   [pscustomobject]@{}
 }
