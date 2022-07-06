fae9fe38ea017492281398c072de67bde93d9eb1
Tanyaapurva <apurva.tanya17@gmail.com>
2022-07-05 19:56:29 -0500

    Update maintenance_handler.py

--- v2/cloud_functions/handlers/maintenance_handler.py
+++ v2/cloud_functions/handlers/maintenance_handler.py
@@ -1,10 +1,15 @@
 import logging
 from calendar import calendar
-from datetime import time
+from datetime import time, datetime
 
+import googleapiclient
 from google import pubsub_v1
 from google.cloud import logging_v2
+from googleapiclient.errors import HttpError
+from joblib import disk
+from joblib.testing import timeout
 
+from cloud_fn_utilities.gcp.datastore_manager import DataStoreManager
 from cloud_fn_utilities.globals import PubSub, DatastoreKeyTypes, BuildConstants
 from cloud_fn_utilities.gcp.cloud_env import CloudEnv
 from cloud_fn_utilities.gcp.compute_manager import ComputeManager
@@ -18,19 +23,34 @@ __maintainer__ = "Philip Huff"
 __email__ = "pdhuff@ualr.edu"
 __status__ = "Testing"
 
+from cloud_fn_utilities.server_specific.fixed_arena_workspace_proxy import FixedArenaWorkspaceProxy
+from cloud_fn_utilities.state_managers.fixed_arena_states import FixedArenaStateManager
+
+from cloud_fn_utilities.state_managers.server_states import ServerStateManag
+
 from common.globals import log_client, compute, project, zone, LOG_LEVELS, ds_client, BUILD_STATES, PUBSUB_TOPICS, \
-    WORKOUT_TYPES, cloud_log, workout_globals, SERVER_STATES
+    WORKOUT_TYPES, cloud_log, workout_globals, SERVER_STATES, SERVER_ACTIONS, dns_suffix, parent_project, dnszone, \
+    gcp_operation_wait
 from common.state_transition import state_transition
 
 
 
 class MaintenanceHandler:
     def __init__(self, event_attributes):
+        self.state_manager = None
+        self.server_name = event_attributes
+        self.fixed_arena_workspace_ids = None
         self.env = CloudEnv()
         self.DEFAULT_LOOKBACK = 10512000
+#        self.lookback_seconds = self.lookback_seconds
         log_client = logging_v2.Client()
         log_client.setup_logging()
         self.event_attributes = event_attributes
+        self.s = ServerStateManag.States
+        self.server_spec = DataStoreManager(key_type=DatastoreKeyTypes.SERVER, key_id=self.event_attributes).get()
+        self.compute = googleapiclient.discovery.build('compute', 'v1')
+        self.server_states = ServerStateManag.States
+        #self.lookback_seconds = self.lookback_seconds
 
     def route(self):
         action = self.event_attributes('action', None)
@@ -47,7 +67,51 @@ class MaintenanceHandler:
             logging.error(f"Unsupported action supplied to the maintenance handler")
             raise ValueError
 
-    def delete_server(self, build_id) -> object:
+    def _delete_expired_workouts(self):
+
+        query_old_workouts = ds_client.query(kind='cybergym-workout')
+        query_old_workouts.add_filter('active', '=', True)
+
+        for workout in list(query_old_workouts.fetch()):
+            workout_project = workout.get('build_project_location', project)
+            if workout_project == project:
+                if 'state' not in workout:
+                    workout['state'] = BUILD_STATES.DELETED
+                container_type = False
+                # If the workout is a container, then expire the container so it does not show up in the list of running workouts.
+                if 'build_type' in workout and workout['build_type'] == 'container':
+                    container_type = True
+                    if 'timestamp' not in workout or 'expiration' not in workout and \
+                            workout['state'] != BUILD_STATES.DELETED:
+                        state_transition(workout, BUILD_STATES.DELETED)
+                    elif self._workout_age(workout['timestamp']) >= int(workout['expiration']) and \
+                            workout['state'] != BUILD_STATES.DELETED:
+                        state_transition(workout, BUILD_STATES.DELETED)
+
+                arena_type = False
+                if 'build_type' in workout and workout['build_type'] == 'arena' or \
+                        'type' in workout and workout['type'] == 'arena':
+                    arena_type = True
+
+                if not container_type and not arena_type:
+                    if self._workout_age(workout['timestamp']) >= int(workout['expiration']):
+                        if workout['state'] != BUILD_STATES.DELETED:
+                            state_transition(workout, BUILD_STATES.READY_DELETE)
+                            workout_id = workout.key.name
+                            # Delete the workouts asynchronously
+                            pubsub_topic = PUBSUB_TOPICS.DELETE_EXPIRED
+                            publisher = pubsub_v1.PublisherClient()
+                            topic_path = publisher.topic_path(project, pubsub_topic)
+                            publisher.publish(topic_path, data=b'Workout Delete', workout_type=WORKOUT_TYPES.WORKOUT,
+                                              workout_id=workout_id)
+
+    def _workout_age(self, created_date):
+        now = datetime.now()
+        instance_creation_date = datetime.fromtimestamp(int(created_date))
+        delta = now - instance_creation_date
+        return delta.days
+
+    def delete_server(self, build_id):
         #query_old_units = ds_client.query(kind = 'cybergym-unit')
         #query_old_units.add_filter("timestamp", ">", str(calendar.timegm(time.gmtime()) - self.DEFAULT_LOOKBACK))
         query_old_units = ds_client.query(kind='cybergym-workout')
@@ -91,54 +155,45 @@ class MaintenanceHandler:
             }, severity=LOG_LEVELS.INFO
         )
 
-    def start(self):
-        """
-        Starts a server based on the specification in the Datastore entity with name server_name. A guacamole server
-        is also registered with DNS.
-        """
-        state_manager = ServerStateManager(initial_build_id=self.server_name)
-        state_manager.state_transition(self.s.STARTING)
+    def create_snapshot(self,  max_snapshots=3, server_name=None):
+
+        service = googleapiclient.discovery.build('compute', 'v1')
+
+
+        found_available = False
+        latest = 0
+        latest_ts = '2000-01-01T00:00:00.000'
         i = 0
-        start_success = False
-        while not start_success and i < 5:
+
+
+        while i < int(max_snapshots):
             try:
-                if "delayed_start" in self.server_spec and self.server_spec["delayed_start"]:
-                    time.sleep(30)
-                response = self.compute.instances().start(project=self.env.project, zone=self.env.zone,
-                                                          instance=self.server_name).execute()
-                start_success = True
-                logging.info(f'Sent job to start {self.server_name}, and waiting for response')
-            except BrokenPipeError:
+                response = service.snapshots().get(project=project, snapshot=server_name + str(i)).execute()
+                if response['creationTimestamp'] > latest_ts:
+                    latest_ts = response['creationTimestamp']
+                    latest = i
                 i += 1
-        i = 0
-        success = False
-        while not success and i < 5:
-            try:
-                self.compute.zoneOperations().wait(project=self.env.project, zone=self.env.zone,
-                                                   operation=response["id"]).execute()
-                success = True
-            except timeout:
-                i += 1
-                logging.warning(f'Response timeout for starting server {self.server_name}. Trying again')
-                pass
-        if not success:
-            logging.error(f'Timeout in trying to start server {self.server_name}')
-            state_manager.state_transition(self.s.BROKEN)
-            raise ConnectionError
+            except HttpError:
+                latest = (i - 1) % max_snapshots
+                found_available = True
+                break
 
-        # If the server is an external proxy, then register its DNS name
-        dns_hostname = self.server_spec.get('dns_hostname', None)
-        if dns_hostname:
-            dns_record = dns_hostname + self.env.dns_suffix + "."
-            self.dns_manager.add_dns_record(dns_record, self.server_name)
+        selected_snapshot = f"{server_name}{(latest + 1) % max_snapshots}"
+        if not found_available:
+            response = service.snapshots().delete(project=project, snapshot=selected_snapshot).execute()
 
-        state_manager.state_transition(self.s.RUNNING)
-        logging.info(f"Finished starting {self.server_name}")
+            if not gcp_operation_wait(service=service, response=response, wait_type="global"):
+                raise Exception(f"Timeout waiting for snapshot {selected_snapshot} to delete.")
 
-    def nuke(self):
-        """
-        Deletes a server based on the specification in the Datastore entity with the name server_name. Then rebuilds
-        the server.
-        """
-        self.delete()
-        self.build()
+        snapshot_body = {
+            'name': selected_snapshot,
+            'description': 'Production snapshot used for imaging'
+        }
+
+        response = service.disks().createSnapshot(project=project, zone=zone, disk=server_name, body=snapshot_body).execute()
+
+        if not gcp_operation_wait(service=service, response=response):
+            raise Exception(f"Timeout waiting for snapshot {selected_snapshot} to be created")
+
+        return selected_snapshot
+
