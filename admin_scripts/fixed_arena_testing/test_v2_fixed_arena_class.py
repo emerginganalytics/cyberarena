d567727184f5899f63fe78320e0f883e463db72b
Philip Huff <pdhuff@ualr.edu>
2022-08-10 14:13:37 -0500

    Fixing start/stop functions for fixed-arena-classroom

--- admin_scripts/fixed_arena_testing/test_v2_fixed_arena_class.py
+++ admin_scripts/fixed_arena_testing/test_v2_fixed_arena_class.py
@@ -2,7 +2,7 @@ from google.cloud import datastore
 from datetime import datetime, timedelta, timezone
 import yaml
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-from main_app_utilities.globals import Buckets
+from main_app_utilities.globals import Buckets, PubSub
 from main_app_utilities.gcp.cloud_env import CloudEnv
 from main_app_utilities.gcp.bucket_manager import BucketManager
 from main_app_utilities.infrastructure_as_code.build_spec_to_cloud import BuildSpecToCloud
@@ -21,12 +21,16 @@ __status__ = "Production"
 
 # Parse command line arguments
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-parser.add_argument("-a", "--fixed-arena", default=None, help="Fixed Arena Specification to build")
+parser.add_argument("-a", "--fixed-arena-class", default=None, help="Fixed Arena Specification to build")
+parser.add_argument("-b", "--build_id", default=None, help="Build ID")
+parser.add_argument("-x", "--action", default=None, help="Action to take")
 
 args = vars(parser.parse_args())
 
 # Set up parameters
-fixed_arena_class = args.get('fixed_arena_class')
+fixed_arena_class = args.get('fixed-arena-class', None)
+build_id = args.get('build_id', None)
+action = args.get('action', None)
 
 
 class TestFixedArenaWorkout:
@@ -49,7 +53,17 @@ class TestFixedArenaWorkout:
         fac = FixedArenaClass(build_id=build_spec['id'], debug=True)
         fac.build()
 
+    def start(self):
+        FixedArenaClass(build_id=build_id, debug=True).start()
+
+    def stop(self):
+        FixedArenaClass(build_id=build_id, debug=True).stop()
 
 if __name__ == "__main__":
     fixed_arena_class = 'stoc-class' if not fixed_arena_class else fixed_arena_class
-    TestFixedArenaWorkout().build()
+    if not action or action == PubSub.Actions.BUILD.name:
+        TestFixedArenaWorkout().build()
+    elif action == PubSub.Actions.START.name:
+        TestFixedArenaWorkout().start()
+    elif action == PubSub.Actions.STOP.name:
+        TestFixedArenaWorkout().stop()
