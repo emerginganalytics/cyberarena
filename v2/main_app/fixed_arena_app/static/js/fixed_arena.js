f1d8a4e2de235f90c5cf39606ad88b5e7966d305
abbomberger <50710945+abbomberger@users.noreply.github.com>
2022-08-19 15:42:16 -0500

    Updates to front end stoc

--- v2/main_app/fixed_arena_app/static/js/fixed_arena.js
+++ v2/main_app/fixed_arena_app/static/js/fixed_arena.js
@@ -1,9 +1,3 @@
-$(document).ready(function() {
-    createStocManager();
-    createClassManager();
-    vulnTemplateManager();
-});
-
 function send_request(args){
     // Build the URL
     let url = '';
@@ -89,6 +83,13 @@ function send_request(args){
         console.log(err);
     }
 }
+function toggleServerControl(class_name){
+    // Enables the server control buttons based on class
+    let target_buttons = document.getElementsByClassName(class_name);
+    for (let i=0, n=target_buttons.length; i < n; i++){
+        enable_object(target_buttons[i].id, target_buttons[i].checked);
+    }
+}
 function enable_object(obj_id, enable=false, clear=false) {
     let obj = $('#' + obj_id);
     if (typeof enable == "boolean") {
@@ -103,10 +104,64 @@ function enable_object(obj_id, enable=false, clear=false) {
     }
 }
 function select_all(caller, class_name){
-    // Takes in caller obj and class name to search for.
+    // Takes in caller obj and class name to search for and selects all checkboxes
+    // containing the input class name.
     let checkboxes = document.getElementsByClassName('' + class_name);
     for (let i=0, n=checkboxes.length; i < n; i++){
         checkboxes[i].checked = caller.checked;
+    }
+}
+function setDatetimeLimits(){
+    let today = new Date();
+    let day = today.getDay();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    if (day < 10) {
+        day = '0' + day;
+    }
+    if (month < 10) {
+        // Max date allowed for classes is 3 months
+        if (month + 3 >= 10) {
+            month = month + 3;
+        }
+        else {
+            month = '0' + month;
+        }
+    }
+    let min_date = year + '-' + month + '-' + day;
+    let max_date = year + '-' + month + '-' + day;
+    document.getElementById('class-expire-date').setAttribute("min", min_date);
+    document.getElementById('class-expire-date').setAttribute("max", max_date);
+}
+function waitingAnimation(class_name, build_id, build_type){
+    // Takes input class name and adds a loading animation for each returned element.
+    // Once the condition is met, refresh the page
+    let url = '/api/fixed-arena'
+    let status_obj = document.getElementsByClassName(class_name);
+    for (let i = 0; i < status_obj.length; i++ ){
+        // clear the object
+        status_obj[i].innerHTML = "";
+        // create and insert loading animation object
+        let spinner = document.createElement('span');
+        spinner.className = 'spinner-border spinner-border-sm';
+        spinner.setAttribute('aria-hidden', 'true');
+        spinner.setAttribute('role', 'status');
+        status_obj[i].append(spinner);
+    }
+    if (build_type === 'class'){
+        let url = '/fixed-arena/' + build_type + '/' + build_id;
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        })
+            .then(response => response.json())
+            .then(response => function () {
+            })
+    } else if (build_type === 'fixed-arena'){
+        url = url + '/' + build_id;
     }
 }
 function manage_stoc(action){
@@ -158,6 +213,7 @@ function manage_class(action, build_id=null) {
     console.log(build_id)
     console.log(action)
     if (action === 2 || action === 4) {
+        waitingAnimation('class-status');
         let body_data = {'action': action}
         args = {'build_type': 'class', 'build_id': build_id, 'method': 'PUT', 'data': body_data}
         send_request(args);
@@ -192,132 +248,6 @@ function createClassManager(){
             send_request(args)
         });
     }
-}
-function vulnTemplateManager(){
-       // TODO: Remove display number rows option; Set default value to 10 with overflow being sent to
-       //       new page
-    $('#vuln-templates-table').DataTable({
-        "paging": false,
-        "bInfo": false,
-        "columns": [
-            {searchable: true, orderable: true},
-            {searchable: true, orderable: false},
-            {searchable: true, orderable: true},
-            {searchable: false, orderable: false},
-        ],
-        "dom": '<"table_title">frtip',
-    });
-    $("div.table_title").html('<h3>Select an Attack Template: </h3>');
-
-    // Resets form if modal close or cancel buttons are clicked
-    $('button.cancel-form').click(function(){
-        var table = $('#vuln-templates-table');
-        table.find('.selected').removeClass('selected');
-        table.find('.checkmark').removeClass('checkmark');
-        enable_object('vuln-template-btn');
-    });
-
-    // Vuln form listener
-    $('#vuln-template-form').submit(function(e){
-        e.preventDefault();
-        let post_url = $(this).attr('action');
-        let request_method = $(this).attr("method");
-        let serialized = $(this).serialize().split("&");
-
-        // build json object from serialized form
-        let form_data = {}
-        for (var key in serialized){
-            form_data[serialized[key].split("=")[0]] = serialized[key].split("=")[1];
-        }
-        form_data['stoc_id'] = 'temp_stoc_id';
-        console.log(form_data);
-        $('#vuln-form-modal').modal('hide');
-
-        let args = {'url': post_url, 'method': request_method, 'data': form_data}
-        send_request(args);
-        /*
-        $.ajax({
-            type: request_method,
-            contentType: 'application/json;charset=utf-8',
-            traditional: true,
-            url: post_url,
-            data: JSON.stringify(form_data),
-            processData: false,
-            dataType: "json",
-            success: function (data){
-                console.log(data);
-            },
-            error: function(jqXHR, textStatus, errorThrown){
-                console.log(textStatus);
-                console.log(errorThrown);
-            }
-       });
-       */
-    });
-}
-function build_form(data){
-    // Builds form with data received from vuln-template-table POST request
-    let vulnForm = $('#vuln-template-args');
-    // First clean up any old form values
-   document.getElementById('vuln-template-args').innerHTML = '';
-
-   // Create field for attack_id
-   let aid_div = document.createElement('div');
-   aid_div.className = 'form-group';
-   // Attack ID Input
-   let aid_input = document.createElement('input');
-   aid_input.type = 'text';
-   aid_input.name = 'attack_id';
-   aid_input.value = data.id;
-   aid_input.readOnly = true;
-   aid_input.className = 'readOnly';
-   // Attack ID label
-   let aid_label = document.createElement('label');
-   aid_label.htmlFor = 'attack_id';
-   aid_label.innerText = 'Attack ID: ';
-   // Add fields to form
-   aid_div.appendChild(aid_label);
-   aid_div.appendChild(aid_input);
-   vulnForm.append(aid_div);
-
-    // Create arg label and select field
-   for (let arg = 0; arg < data.args.length; arg++){
-       if (data.args[arg].id !== 'target_network'){
-           // Create wrapper div
-           let arg_div = document.createElement('div');
-           arg_div.className = 'form-group';
-           // Create arg select field
-           let arg_select = document.createElement("select");
-           arg_select.name = data.args[arg].id;
-           arg_select.id = data.args[arg].id;
-           // Create arg label
-           var arg_label = document.createElement('label');
-           arg_label.htmlFor = data.args[arg].id;
-           arg_label.textContent = data.args[arg].name + ':';
-           if ('Choices' in data.args[arg]){
-               for (let i = 0; i < data.args[arg].Choices.length; i++){
-                    let option_i = document.createElement('option');
-                    let option_key = Object.keys(data.args[arg]['Choices'][i])[0];
-                    option_i.name = option_key;
-                    option_i.value = option_key.toString();
-                    option_i.textContent = option_key;
-
-                    // check for default value
-                    let current_choice = data['args'][arg]['Choices'][i];
-                    if (current_choice === data.args[arg].default){
-                        option_i.defaultSelected = true;
-                    }
-                    arg_select.appendChild(option_i);
-               } // end Choices for loop
-               // Append generated fields to form
-               arg_div.appendChild(arg_label);
-               arg_div.appendChild(arg_select);
-               vulnForm.append(arg_div);
-           }
-       }  // end !target_network
-   } // end args for loop
-   // Form is built; Toggle form modal
-    $('#vuln-form-modal').modal();
 }
 function copy_student_links(){
     let temp_div = document.createElement("textarea");
