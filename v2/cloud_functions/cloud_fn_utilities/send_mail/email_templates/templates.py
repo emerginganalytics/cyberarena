108ed0bb343c3c671d38b8ef7a5a0ae934a94354
RyanEbsen <77689652+RyanEbsen@users.noreply.github.com>
2022-12-18 21:02:09 -0600

    Fixed send_mail string

---
+++ v2/cloud_functions/cloud_fn_utilities/send_mail/email_templates/templates.py
@@ -0,0 +1,22 @@
+import os
+from sendgrid.helpers.mail import *
+from enum import Enum
+
+
+class Subjects(Enum):
+    Expired = "Virtual Machine About to Expire"
+
+
+class Templates:
+    def __init__(self):
+        self.subjects = Subjects
+
+    def get_template(self, eml_subject):
+        if eml_subject.value == self.subjects.Expired.value:
+            filepath = os.path.dirname(os.path.abspath(__file__))
+            file = open(f"{filepath}/expired.txt", "r")
+            eml_content = Content(
+                content=file.read(),
+                mime_type="text/plain"
+            )
+            return eml_content
