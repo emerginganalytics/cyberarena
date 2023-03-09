fc4eebf5e14f3e301a6fb690b251e74febaf1d8e
Andrew <abbomberger@gmail.com>
2023-03-09 01:34:13 -0600

    Updates to classified blueprint

--- v2/container-applications/Classified/app.py
+++ v2/container-applications/Classified/app.py
@@ -1,10 +1,4 @@
-import base64
-import onetimepass
-import os
 from flask import Flask, redirect, abort, jsonify, render_template, request, session
-from flask_bootstrap import Bootstrap
-from flask_login import LoginManager, UserMixin
-from flask_sqlalchemy import SQLAlchemy
 
 # App imports
 from app_utilities.gcp.datastore_manager import DataStoreManager
@@ -13,8 +7,8 @@ from app_utilities.globals import DatastoreKeyTypes
 from app_utilities.crypto_suite.hashes import Hashes
 
 # App Blueprint Imports
+from classified.routes import classified_bp
 from johnnyhash.routes import johnnyhash_bp
-
 
 # API Views
 from api.johnnyhash_api import JohnnyHashAPI, JohnnyCipherAPI
@@ -25,16 +19,10 @@ cloud_env = CloudEnv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'XqLx4yk8ZW9uukSCXIGBm0RFFJKKyDDm'
 app.jinja_env.globals['project'] = cloud_env.project
-bootstrap = Bootstrap(app)
-
-# db Config
-db = SQLAlchemy(app)
-lm = LoginManager(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Register Blueprints
 app.register_blueprint(johnnyhash_bp)
+app.register_blueprint(classified_bp)
 
 
 @app.route('/<build_id>')
@@ -45,7 +33,7 @@ def home(build_id):
         if web_app:
             directory = web_app[0].get('starting_directory', None)
             if directory:
-                return redirect(directory)
+                return redirect(f'{directory}/{build_id}')
     return redirect('/invalid')
 
 
---
+++ v2/container-applications/Classified/classified/config.py
@@ -0,0 +1,75 @@
+import base64
+import sqlite3
+import onetimepass
+import os
+import hashlib
+import sqlite3
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+
+class RegisterForm(FlaskForm):
+    """Registration form."""
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Confirm Password',
+                                   validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    token = StringField('Token', validators=[DataRequired(), Length(6, 6)])
+    submit = SubmitField('Login')
+
+
+class SQLInjection:
+    def __init__(self):
+        self.db = 'bad.db'
+
+    def connect(self):
+        return sqlite3.connect(self.db)
+
+    @staticmethod
+    def _hash_pass(pwd):
+        m = hashlib.md5()
+        m.update(pwd.encode('utf-8'))
+        return m.hexdigest()
+
+    def _populate(self):
+        with self.connect() as connection:
+            c = connection.cursor()
+            c.execute("""CREATE TABLE employees(username TEXT, password TEXT)""")
+            c.execute('INSERT INTO employees VALUES("RickAstley", "{}")'.format(self._hash_pass("NeverGonnaGiveYouUp")))
+            c.execute('INSERT INTO employees VALUES("SuperSecretClassifiedEmployee", "{}")'.format(self._hash_pass("password")))
+            c.execute('INSERT INTO employees VALUES("BigBossAdmin", "{}")'.format(self._hash_pass("kittycat222")))
+            connection.commit()
+
+
+class XSS:
+    def __init__(self):
+        self.db = 'xss_s.db'
+
+    def connect(self):
+        xss_db = sqlite3.connect(self.db)
+        xss_db.cursor().execute("""CREATE TABLE IF NOT EXISTS feedbacks (workout_ids TEXT, feedback TEXT)""")
+        xss_db.commit()
+        return xss_db
+
+    def add_feedback(self, feedback, workout_id):
+        xss_db = self.connect()
+        xss_db.cursor().execute("""INSERT INTO feedbacks (workout_ids, feedback) VALUES (?, ?) """, (workout_id, feedback,))
+        xss_db.commit()
+
+    def get_feedback(self, workout_id, search_query=None):
+        xss_db = self.connect()
+        results = []
+        for (feedback,) in xss_db.cursor().execute("""SELECT feedback FROM feedbacks WHERE workout_ids=?""", (workout_id,)).fetchall():
+            if search_query is None or search_query in feedback:
+                results.append(feedback)
+        return results
