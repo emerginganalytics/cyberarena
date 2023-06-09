89fe71b13afa65c4f85fff567d864aab4fbd1a38
Benjamin Miller <benjamin.miller2001@gmail.com>
2023-06-09 11:54:04 -0500

    update password-policy.yaml

--- v2/build_files/startup_scripts/password-policy.ps1
+++ v2/build_files/startup_scripts/password-policy.ps1
@@ -1,10 +1,8 @@
 ﻿if((((Get-ADDefaultDomainPasswordPolicy).MinPasswordLength) -eq 12) -and (((Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge.Days) -eq 365) -and (((Get-ADDefaultDomainPasswordPolicy).ComplexityEnabled) -eq "True")){
 
-    $workout = @{
-        workout_id=$env:WORKOUTID;
-        token=$env:WORKOUTKEY0;
-    }| ConvertTo-Json
-    $complete_url = -join("https://buildthewarrior", $env:DNS_SUFFIX, "/complete");
-    Invoke-WebRequest -UseBasicParsing $complete_url -Method POST -Body $workout -ContentType "application/json"
+    Write-Output "True"
 
+}
+else{
+    Write-Output "False"
 }
--- v2/build_files/startup_scripts/password-policy.py
+++ v2/build_files/startup_scripts/password-policy.py
@@ -2,32 +2,25 @@
 import os
 import time
 import requests
-import pwd
 import subprocess
-import pyad
+import sys
 
 
-class AssessmentArtifacts:
-    Q0_USER_CHECK = 'gigabyte'
-    OLD_SOFTWARE = "liblog4j2-java/bionic,now 2.10.0-2"
-    FILETYPE_CHECK = 'mp4'
-    ADMIN_CHECK = 'philip'
-    SUDOERS_STRING = "(ALL : ALL) ALL"
-    CRONTAB_STRING = "philip    ALL=(ALL:ALL) ALL"
+def run():
+    powershell_script = '''
+    if((((Get-ADDefaultDomainPasswordPolicy).MinPasswordLength) -eq 12) -and (((Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge.Days) -eq 365) -and (((Get-ADDefaultDomainPasswordPolicy).ComplexityEnabled) -eq "True")){
+        Write-Output "True"
+    }
+    else{
+        Write-Output "False"
+    }
+    '''
 
+    process = subprocess.Popen(["powershell", "-Command", powershell_script], stdout=subprocess.PIPE)
+    result = process.communicate()[0]
 
-def directory_setup():
-    pyad.set_defaults(ldap_server='cybergym.com')
-    ad_query = pyad.adquery.ADQuery()
-    ad_query.execute_query(
-        attributes=["maxPwdAge", "minPwdLength", "complexityEnabled"],
-        where_clause="objectClass='domain'"
-    )
-    for row in ad_query.get_results():
-        max_password_age = row["maxPwdAge"]
-        minimum_password_length = row["minPwdLength"]
-        password_complexity = row["complexityEnabled"]
-    return[max_password_age, minimum_password_length, password_complexity]
+    output = result.decode("utf-8")
+    return output
 
 
 class Question:
@@ -66,11 +59,8 @@ class Question0(Question):
         super().__init__(build_id, url, 0)
 
     def unique_assessment(self):
-        properties = directory_setup()
-        max_password_age = properties[0]
-        minimum_password_length = properties[1]
-        password_complexity = properties[2]
-        if max_password_age == 365 and minimum_password_length == 12 and password_complexity is True:
+        properties = run()
+        if properties == "True":
             return True
         return False
 
